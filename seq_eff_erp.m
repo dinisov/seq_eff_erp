b24b078e0b157d340d4a2c57fb74fee6d639f98c
dinisov <dgokaydin@gmail.com>
2024-01-23 16:35:15 +1000

    Mean TF across flies

--- seq_eff_erp.m
+++ seq_eff_erp.m
@@ -78,7 +78,7 @@ whichFly =      fly_record.Fly.';
 flySet = unique(whichFly);
 
 % choose which flies to run here
-chosenFlies = [125];
+chosenFlies = [124 125];
 % chosenFlies = setdiff(flySet, [24 25]);
 %  chosenFlies = flySet; % choose all flies
 % chosenFlies = setdiff(chosenFlies, 24:29);
@@ -90,7 +90,7 @@ chosenFlies = [125];
 %NOTE: while unlikely as a request, this does not handle the case where two
 %flies have a block with the same number but we would like to look at both
 %flies but not one of the blocks with the same number
-chosenBlocks = [15];
+chosenBlocks = [8 15];
 %  chosenBlocks = unique(fly_record.Block.');% do not choose specific blocks
 
 chosenOnes = ismember(fly_record.Block.', chosenBlocks) & ismember(fly_record.Fly.', chosenFlies);
